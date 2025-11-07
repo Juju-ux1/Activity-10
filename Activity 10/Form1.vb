69f327af61b34950084d6605351e2a08e4ec23f7
Juju-ux1 <hed-jcutaran@smu.edu.ph>
2025-11-07 18:54:05 +0800

    removed comment

--- Activity 10/Form1.vb
+++ Activity 10/Form1.vb
@@ -5,30 +5,30 @@ Public Class Form1
         Try
             Dim input As String = txtNumbers.Text
 
-            ' Write to file
+
             Using writer As New StreamWriter("numbers.txt")
                 writer.WriteLine(input)
             End Using
 
-            ' Read from file
+
             Dim fileContent As String
             Using reader As New StreamReader("numbers.txt")
                 fileContent = reader.ReadToEnd()
             End Using
 
-            ' Convert to list of integers
+
             Dim numbers As List(Of Integer) = fileContent.Split(","c).Select(Function(n) Integer.Parse(n.Trim())).ToList()
 
-            ' Sort numbers using LINQ OrderBy
+
             Dim sortedNumbers = numbers.OrderBy(Function(n) n).ToList()
 
-            ' Display in ListBox
+
             Sorted.Items.Clear()
             For Each num In sortedNumbers
                 Sorted.Items.Add(num)
             Next
 
-            ' Also show in Console
+
             Console.WriteLine("Sorted Numbers:")
             For Each num In sortedNumbers
                 Console.WriteLine(num)
